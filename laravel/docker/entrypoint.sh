ecaa20b08bf34274553e149fe98b8db80e09d19c
rasvanjaya21 <mrsejati17@gmail.com>
2026-04-07 15:19:08 +0700

    feat(laravel): update laravel version to 0.1.2

--- laravel/docker/entrypoint.sh
+++ laravel/docker/entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 if [ ! "$(ls -A /var/www/storage)" ]; then
   echo "Initializing storage directory..."
   cp -R /var/www/storage-init/. /var/www/storage
-  chown -R www-data:www-data /var/www/storage
+  chown -R circleit:circleit /var/www/storage
 fi
 
 rm -rf /var/www/storage-init
